bd3abed9d15b6cec0816c83f64fc7a674a1dbb83
User <User@Lenovo-PC>
2022-08-17 08:48:24 +0530

    Today is

--- SeleniumBasics/src/main/java/seleniumexamples/Demoxpath.java
+++ SeleniumBasics/src/main/java/seleniumexamples/Demoxpath.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demoxpath {
 
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\Browser\\Chromedriver\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
